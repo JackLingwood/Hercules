920ddebc3a03129035aeb64034418f9d3943d2d6
JackLingwood <Jack.Lingwood@Gmail.com>
2018-12-21 12:22:05 -0500

    2nd checkin of a comment

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+// Comment
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
