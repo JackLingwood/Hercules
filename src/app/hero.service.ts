6126eded842c26bfd2ea6444c8138b5729309428
JackLingwood <Jack.Lingwood@Gmail.com>
2019-01-03 14:58:41 -0500

    Completed Routing component of Tour of Heroes tutorial.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,10 +6,10 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
+
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
@@ -17,6 +17,9 @@ export class HeroService {
     return of(HEROES);
   }
 
-  constructor(private messageService: MessageService) { }
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
-
